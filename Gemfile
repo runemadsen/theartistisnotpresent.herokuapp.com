14643249839597282e074e946ed30eb514163a15
Rune Skjoldborg Madsen <rune@runemadsen.com>
2013-10-08 17:22:00 -0400

    lots of changes

--- Gemfile
+++ Gemfile
@@ -2,9 +2,11 @@ source 'https://rubygems.org'
 ruby '1.9.3'
 
 gem 'sinatra'
+gem 'dropbox-sdk', :require => "dropbox_sdk"
 
 group :development do
 	gem 'shotgun'
+  gem 'dotenv'
 end
 
 group :production do
--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require 'rubygems'
 require 'bundler'
 Bundler.require
+require 'json'
+  
+if settings.environment == :development
+  Bundler.require(:development)
+  Dotenv.load
+end
 
 set :root, File.dirname(__FILE__)
 
 get '/' do
-  erb :hello
+  erb :front
+end
+
+get '/files' do
+  client = DropboxClient.new(ENV['DROPBOX_ACCESS_TOKEN'])
+  files = client.metadata('/Public')
+  files.to_json
 end
