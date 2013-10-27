16e976b170bb45a07d21f8bd33c85fd11af867f9
Rune Skjoldborg Madsen <rune@runemadsen.com>
2013-10-26 21:03:32 -0400

    adding folder structure

--- app.rb
+++ app.rb
@@ -16,13 +16,24 @@ set :root, File.dirname(__FILE__)
 set :cache, Dalli::Client.new(ENV["MEMCACHIER_SERVERS"], {:expires_in => 60, :username => ENV["MEMCACHIER_USERNAME"], :password => ENV["MEMCACHIER_PASSWORD"]})
 
 get '/' do
+  @js_data = {
+    :endpoint => ENV['DROPBOX_ENDPOINT']
+  }
   erb :front
 end
 
-get '/files' do
-  @dropboxfiles ||= settings.cache.fetch("dropboxfiles") do
+get '/run/:folder' do
+  @js_data = {
+    :endpoint => ENV['DROPBOX_ENDPOINT'],
+    :folder => params[:folder]
+  }
+  erb :run
+end
+
+get '/files/?:folder?' do
+  @dropboxfiles ||= settings.cache.fetch("cached-#{params[:folder]}") do
     client = DropboxClient.new(ENV['DROPBOX_ACCESS_TOKEN'])
-    files = client.metadata('/Public')
+    files = client.metadata("/Public/#{params[:folder]}")
     files.to_json
   end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,18 +1,75 @@
-$(function() {
+var app = {
 
-  // load JSON
-  $.getJSON( "/files", function(data) {
-    
-    $.each(data.contents, function(index, content) {
-      
-      var path_split = content.path.split("/")
-      var path_name = path_split[path_split.length-1];
-      var name_split = path_name.split("-");
-      
-      $('<figure><img src="'+window.endpoint+'/'+path_name+'" type="image/svg+xml" /><figcaption>Generation <span class="highlight">'+name_split[0]+'</span> Work <span class="highlight">'+name_split[1]+'</span> '+name_split[2]+'/'+name_split[3]+'/'+name_split[4]+' '+name_split[5]+':'+name_split[6]+'</figcaption></figure>').appendTo(".container");
+  time_template : _.template('<%= year %>/<%= month %>/<%= day %> <%= hour %>:<%= minute %>'),
 
+  svg_template : _.template('<figure><a href="<%= link %>"><img src="<%= endpoint %>/<%= imgsrc %>" type="image/svg+xml" /></a><figcaption><%= label %></figcaption></figure>'),
+
+  render_root : function()
+  {
+    var t = this;
+
+    $.getJSON( "/files", function(data) {
+      
+      $.each(data.contents, function(index, content) {
+        
+        var full_path = content.path.split("/");
+        var folder_name = full_path[full_path.length-1];
+        var folder_split = folder_name.split("_");
+
+        var time_object = {
+          year : folder_split[0],
+          month : folder_split[1],
+          day : folder_split[2],
+          hour : folder_split[3],
+          minute : folder_split[4],
+          second : folder_split[5]
+        };
+
+        var link = "/run/" + folder_name;
+        
+        $(t.svg_template({
+          endpoint : window.js_data.endpoint,
+          imgsrc : folder_name + "/latest.svg",
+          link : link,
+          label : "Artwork generated " + t.time_template(time_object) + ". <a href='"+link+"'>Click to see process</span>."
+        })).appendTo(".container");
+
+      });
+  
+    });
+  },
+
+  render_run : function(folder)
+  {    
+    var t = this;
+
+    $.getJSON( "/files/"+folder, function(data) {
+      
+      $.each(data.contents, function(index, content) {
+        
+        var full_path = content.path.split("/");
+        var svg_file = full_path[full_path.length-1];
+        if(svg_file == "latest.svg") return;
+
+        var svg_split = svg_file.split("-");
+        var generationNum = svg_split[0];
+        var num = svg_split[1].split(".")[0];
+
+        var template_object = {
+          endpoint : window.js_data.endpoint,
+          imgsrc : folder + "/" + svg_file,
+          label : "Generation <span class='highlight'>"+generationNum+"</span> Number <span class='highlight'>"+num+"</span>",
+          link : window.js_data.endpoint + "/" + folder + "/" + svg_file
+        }
+
+        var link = "/run/" + svg_file;
+        
+        $(t.svg_template(template_object)).appendTo(".container");
+
+      });
+  
     });
 
-  });
+  }
 
-});
+};
